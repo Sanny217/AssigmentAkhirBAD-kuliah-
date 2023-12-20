b40066f7fc7630126d69889a5482cdcc560e501c
IHSAN <IHSAN@LAPTOP-HG6F95T7>
2023-12-20 21:00:51 +0700

    Startnya

---
+++ AssigmentAkhir/src/main/Main.java
@@ -0,0 +1,34 @@
+
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	Scene scene;
+	Stage stage;
+	
+	BorderPane bp;
+	GridPane gp;
+	
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ AssigmentAkhir/src/module-info.java
@@ -0,0 +1,5 @@
+
+module AssigmentAkhir {
+	opens main;
+	requires javafx.graphics;
+}
